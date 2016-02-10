b6cca7edf944ff86113fe8e374370b234ac5996b
Ryan Gonzalez <rymg19@gmail.com>
2016-02-10 17:07:01 -0600

    Test fixes

--- tst.c
+++ tst.c
@@ -321,9 +321,10 @@ LIBCUT_TEST(test_cgroup) {
     // (a(b)?)
     rejit_group groups[2];
     const char str1[] = "a", str2[] = "ab";
-    rejit_instruction instrs[] = {{RJ_ICGROUP}, {RJ_ICGROUP}, {RJ_IOPT},
-                                  {RJ_IWORD, (intptr_t)"a"}, {RJ_INULL}};
-    instrs[0].value = instrs[1].value = (intptr_t)&instrs[4];
+    rejit_instruction instrs[] = {{RJ_ICGROUP}, {RJ_IWORD, (intptr_t)"a"},
+                                  {RJ_ICGROUP}, {RJ_IOPT},
+                                  {RJ_IWORD, (intptr_t)"b"}, {RJ_INULL}};
+    instrs[0].value = instrs[2].value = (intptr_t)&instrs[5];
     rejit_matcher m = rejit_compile_instrs(instrs, 0);
 
     memset(groups, 0, sizeof(groups));
